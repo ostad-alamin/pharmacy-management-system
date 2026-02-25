5e1bce3c6c466768f71108f6ed6fdf81d51001a4
Alamin <alamin.dhurbin@gmail.com>
2026-02-25 16:31:55 +0600

    first commit

---
+++ src/main/java/com/medeasy/pharmacymanagementsystem/PharmacyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.medeasy.pharmacymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medeasy/pharmacymanagementsystem/PharmacyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeasy.pharmacymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
