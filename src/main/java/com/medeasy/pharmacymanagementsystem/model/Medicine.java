92a4663b725b704bbab6d8bc1889c4335902702a
Alamin <alamin.dhurbin@gmail.com>
2026-02-25 17:06:42 +0600

    packeges are created

--- src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
+++ src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
@@ -1,7 +1,11 @@
 package com.medeasy.pharmacymanagementsystem.model;
 
+import jakarta.persistence.Id;
+
 public class Medicine {
 
+    @Id
+    private Long medicineId;
     private String medicineName;
 }
 
---
+++ src/main/java/com/medeasy/pharmacymanagementsystem/repository/MedicineRepository.java
@@ -0,0 +1,6 @@
+package com.medeasy.pharmacymanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<Long, medicineId> {
+}
