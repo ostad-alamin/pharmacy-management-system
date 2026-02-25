e60b4cd2ed456b11fa8a8429a2dd85cd6f6ba7c2
unknown <jerryjeriomio19@gmail.com>
2026-02-25 17:10:47 +0600

    medicine class complete

--- src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
+++ src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
@@ -1,12 +1,24 @@
 package com.medeasy.pharmacymanagementsystem.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
 
 public class Medicine {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long medicineId;
+
     private String medicineName;
+
+    private String groupName;
+
+    private BigDecimal medicinePrice;
+
+    private Integer medicineQuantity;
 }
 
 
