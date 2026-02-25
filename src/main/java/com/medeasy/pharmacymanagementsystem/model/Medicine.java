5a954301ff4c691f6b9bc31dc090855c0f5bdda6
Alamin <alamin.dhurbin@gmail.com>
2026-02-25 17:18:37 +0600

    fixed repo

--- src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
+++ src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
@@ -1,11 +1,13 @@
 package com.medeasy.pharmacymanagementsystem.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 
+@Entity
 public class Medicine {
 
     @Id
@@ -19,6 +21,46 @@ public class Medicine {
     private BigDecimal medicinePrice;
 
     private Integer medicineQuantity;
+
+    public Long getMedicineId() {
+        return medicineId;
+    }
+
+    public void setMedicineId(Long medicineId) {
+        this.medicineId = medicineId;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public BigDecimal getMedicinePrice() {
+        return medicinePrice;
+    }
+
+    public void setMedicinePrice(BigDecimal medicinePrice) {
+        this.medicinePrice = medicinePrice;
+    }
+
+    public Integer getMedicineQuantity() {
+        return medicineQuantity;
+    }
+
+    public void setMedicineQuantity(Integer medicineQuantity) {
+        this.medicineQuantity = medicineQuantity;
+    }
 }
 
 
--- src/main/java/com/medeasy/pharmacymanagementsystem/repository/MedicineRepository.java
+++ src/main/java/com/medeasy/pharmacymanagementsystem/repository/MedicineRepository.java
@@ -1,6 +1,9 @@
 package com.medeasy.pharmacymanagementsystem.repository;
 
+import com.medeasy.pharmacymanagementsystem.model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MedicineRepository extends JpaRepository<Long, medicineId> {
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
 }
