70cae86b5879178415e8efa9445adee27e42392b
Alamin <alamin.dhurbin@gmail.com>
2026-03-13 03:50:19 +0600

    Change

--- src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
+++ src/main/java/com/medeasy/pharmacymanagementsystem/model/Medicine.java
@@ -6,7 +6,6 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-/// ////Change
 @Entity
 @JsonPropertyOrder({"id", "medicineName", "groupName", "medicinePrice", "medicineQuantity"})
 public class Medicine {
